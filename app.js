cf2e85b0a85f0edf2c603ea919b02cdcd69f31b1
Christoph Wiechert <wio@psitrax.de>
2014-10-09 20:47:43 +0200

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+var api = require('./lib/api.js');
+api.version = '0.0.0';
+
+// register API object
+require('./lib/utils.js');
+require('./lib/argv.js');
+require('./lib/logger.js');
+require('./lib/configs.js');
+require('./lib/loader.js');
+
+var logpipes = require('./lib/logpipes');
+
+api.on('ready', function() {
+  if(api.argv.printconfig) {
+    api.utils.printConfig();
+    process.exit(0);
+  }
+
+  // create all logpipes
+  logpipes.init();
+
+  api.logger.info('PsiLog::collector started');
+});
